60d3feec58401c2aaef4ea639349f601ae1bc0ed
Ubuntu <ubuntu@ip-172-31-17-126.ec2.internal>
2014-12-19 15:26:19 +0000

    fix?

--- index.js
+++ index.js
@@ -28,3 +28,24 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/* since didn't include encoding option here
+   readFileSync returns a buffer, so then we need to create
+   a buffer with it and then in response use buf.toString
+   note: not sure if 'utf-8' need to be included here since 
+   encoding when do new Buffer defaults to 'utf8'
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+*/
