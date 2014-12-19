75f8ee7b24109075a05f8e69b6647cfdc721c87c
Ubuntu <ubuntu@ip-172-31-17-126.ec2.internal>
2014-12-19 15:22:12 +0000

    added reference to index.html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+/*var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -10,4 +10,21 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+})*/
+
+var express = require('express');
+var app = express.createServer(express.logger());
+
+// way to do it without a buffer since fs.readFileSync
+// says if you include encoding option ('utf-8') in 
+// this case then this function returns a string
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html', 'utf-8');
+  response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
